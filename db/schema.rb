44c4273ce1c6fe8b0a5bf7d4e53d5d8c345e645b
JefersonCaceres <jefersonkceres@gmail.com>
2022-09-28 23:58:48 -0500

    implementacion de i18n para el idioma es

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_28_060000) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_28_092706) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
@@ -24,6 +24,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_28_060000) do
   create_table "pets", force: :cascade do |t|
     t.string "name"
     t.string "breed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "last_name"
+    t.integer "age"
+    t.text "description"
+    t.integer "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
